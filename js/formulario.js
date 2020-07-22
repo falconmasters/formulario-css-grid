5ecfca56b05e61c941aa7bc239afd9f8f738aa74
Carlos <carlos@falconmasters.com>
2020-07-22 12:00:20 -0500

    Agregando expresiones regulares a codigo base

---
+++ js/formulario.js
@@ -0,0 +1,7 @@
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
